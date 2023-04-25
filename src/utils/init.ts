4dace07b09feb0c6c0e208d2a7401755b030f42e
devisfun201 <devisfun201@gmail.com>
2023-04-25 16:34:08 +0800

    feat: Init a project

---
+++ src/utils/init.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import { IndentationText, Project, QuoteKind } from 'ts-morph'
+export default function init() {
+  dotenv.config()
+  const project = new Project({
+    // Optionally specify compiler options, tsconfig.json, in-memory file system, and more here.
+    // If you initialize with a tsconfig.json, then it will automatically populate the project
+    // with the associated source files.
+    // Read more: https://ts-morph.com/setup/
+    manipulationSettings: {
+      indentationText: IndentationText.Tab,
+      useTrailingCommas: true,
+      quoteKind: QuoteKind.Single,
+    },
+  })
+
+  return project
+}
