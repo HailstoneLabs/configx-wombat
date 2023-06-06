72f409fad5ae37c64e545ad049d1f8c7d3716e0d
devisfun201 <devisfun201@gmail.com>
2023-06-06 17:34:24 +0800

    feat: add overriden token symbol

--- src/constant/common.ts
+++ src/constant/common.ts
@@ -1,7 +1,17 @@
 import * as dotenv from 'dotenv'
+import { SupportedChainId } from '../types/common'
 dotenv.config()
 export const CWD = process.cwd()
 export const CONFIG_FILE_NAME = 'configx.json'
 const DEVELOPMENT_DIR = 'src/example/generatedConfig'
 export const OUTPUT_DIR =
   process.env.ENVIRONMENT === 'STAGING' ? DEVELOPMENT_DIR : 'configx'
+
+// Some symbols on ERC20 contracts are not correct. We can override the symbols here.
+export const OVERRIDEN_TOKEN_SYMBOL: {
+  [chainId in SupportedChainId]?: { [address: string]: string }
+} = {
+  [SupportedChainId.ARBITRUM_ONE]: {
+    '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8': 'USDC.e',
+  },
+}
